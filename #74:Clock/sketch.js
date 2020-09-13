65710f1733596d5c62b679fb90c18710693f4f04
Tae-yoon Kim <chokokty0816@gmail.com>
2020-09-13 20:56:20 +0900

    Update sketch.js

--- #74:Clock/sketch.js
+++ #74:Clock/sketch.js
@@ -7,33 +7,33 @@
 
 function setup() {
   createCanvas(400, 400);
-  angleMode(DEGREES);
+  angleMode(DEGREES); //각 모드 DEGREES RADIANS
 }
 
 function draw() {
   background(0);
   translate(200, 200);
-  rotate(-90);
+  rotate(-90);// 90도 미리 회전시켜놓음
 
-  let hr = hour();
+  let hr = hour(); //p5 library
   let mn = minute();
   let sc = second();
 
   strokeWeight(8);
   stroke(255, 100, 150);
   noFill();
-  let secondAngle = map(sc, 0, 60, 0, 360);
+  let secondAngle = map(sc, 0, 60, 0, 360); //mapping second to angle
   //arc(0, 0, 300, 300, 0, secondAngle);
 
-  stroke(150, 100, 255);
-  let minuteAngle = map(mn, 0, 60, 0, 360);
+  stroke(150, 100, 255); 
+  let minuteAngle = map(mn, 0, 60, 0, 360); //mapping minute to angle
   //arc(0, 0, 280, 280, 0, minuteAngle);
 
   stroke(150, 255, 100);
-  let hourAngle = map(hr % 12, 0, 12, 0, 360);
+  let hourAngle = map(hr % 12, 0, 12, 0, 360); //mapping hour to angle
   //arc(0, 0, 260, 260, 0, hourAngle);
 
-  push();
+  push(); //push pop 사이에만 적용됨
   rotate(secondAngle);
   stroke(255, 100, 150);
   line(0, 0, 100, 0);
